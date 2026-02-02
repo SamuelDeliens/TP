7624b360a4ee0f4b86f0ba88b76225d29ea82413
SamuelDeliens <samuel.deliens@gmail.com>
2026-02-02 21:14:22 +0100

    end

---
+++ tp-front-end/src/components/NavBar.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="nav-bar">
+    <div class="nav-left">
+      <router-link to="/cards"><button>All The Cards</button></router-link>
+      <router-link to="/cardsDetail"><button>Card Detail</button></router-link>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.nav-bar {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 1000;
+  margin: 24px;
+  padding: 12px;
+  display: flex;
+  justify-content: space-between;
+  background: var(--bg-panel);
+  border: 4px solid var(--border-dark);
+  box-shadow: 0 4px 0 var(--border-dark);
+}
+
+
+.nav-left,
+.nav-right {
+  display: flex;
+  gap: 12px;
+}
+
+.nav-bar button {
+  font-family: inherit;
+  background: var(--secondary);
+  color: white;
+  border: 4px solid var(--border-dark);
+  padding: 8px 12px;
+  cursor: pointer;
+  box-shadow: 3px 3px 0 var(--border-dark);
+}
+
+.nav-bar button:hover {
+  background: var(--primary);
+  color: black;
+}
+
+</style>
---
+++ tp-front-end/src/router.ts
@@ -0,0 +1,17 @@
+import CardsPage from "@/pages/CardsPage.vue";
+import CardDetailPage from "@/pages/CardDetailPage.vue";
+import Error404Page from "@/pages/Error404Page.vue";
+import {createRouter, createWebHistory} from "vue-router";
+
+const routes = [
+    { path: '/cards', component: CardsPage },
+    { path: '/cardsDetail', component: CardDetailPage },
+    { path: '/:catchAll(.*)', component: Error404Page },
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: routes,
+})
+
+export default router;
